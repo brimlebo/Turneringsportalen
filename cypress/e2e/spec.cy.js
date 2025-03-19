e09396bb5c61f11a5b6bd695c1bc4e277c61be52
Tord Johansson <tordjoha1@gmail.com>
2025-03-19 10:45:40 +0100

    Revert "added cypress framework and basic test"
    This reverts commit acda9334660caba67d2c46fcea6f73f83adc0062.

--- cypress/e2e/spec.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="Cypress" />
-
-describe('Click on Create Tournament in nav bar', () => {
-  it('Visits Turneringsportalen', () => {
-    cy.visit('https://turneringsportalen-rosy.vercel.app')
-    cy.contains('Create Tournament').click()
-    cy.url().should('include', '/tournaments/create')
-  })
-})
