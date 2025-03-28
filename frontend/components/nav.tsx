615d846145ce227df01b5d26b911c28b824c4f3a
Tord Johansson <tordjoha1@gmail.com>
2025-03-28 13:30:34 +0100

    cleaned up the nav bar, removed unused code. Removed uselessly useless file

--- frontend/components/nav.tsx
+++ frontend/components/nav.tsx
@@ -1,11 +1,10 @@
 'use client'
 
 import { signOutAction } from '@/components/login/actions';
-import { Box, TabNav, Flex, Card, Text, Avatar, Button } from '@radix-ui/themes';
+import { Box, TabNav, Flex, Card, Text, Avatar, Button, Separator } from '@radix-ui/themes';
 import { User } from '@supabase/supabase-js';
 import { usePathname } from 'next/navigation';
 import React from 'react';
-import LoginDialog from './login/LogIn';
 
 export default function Nav({ user }: { user: User | null }) {
   const pathname = usePathname();
@@ -14,22 +13,14 @@ export default function Nav({ user }: { user: User | null }) {
   return (
     <React.Fragment>
       <Flex justify="between">
-
         <TabNav.Root >
           <TabNav.Link active={pathname === '/'} href="/">
             Home
           </TabNav.Link>
-          <TabNav.Link active={pathname === '/tournaments/create'} href="/tournaments/create">
-            Create Tournament
-          </TabNav.Link>
           <TabNav.Link active={pathname === '/tournaments'} href="/tournaments">
             Tournaments
           </TabNav.Link>
-          <TabNav.Link active={pathname === '/tournaments/1/registration'} href="/tournaments/1/registration">
-            Register for tournament (temp)
-          </TabNav.Link>
         </TabNav.Root>
-
         {user ? (
           <Box m="3">
             <Flex direction="row" align="center" gap="5" justify="center">
@@ -48,7 +39,6 @@ export default function Nav({ user }: { user: User | null }) {
                       <Text as="div" size="2"> 
                         {user?.user_metadata.userrole}
                       </Text>
-     
                     </Box>
                   </Flex>
                 </a>
@@ -63,13 +53,6 @@ export default function Nav({ user }: { user: User | null }) {
         ) : (
           <Box m="3">
             <Flex direction="row" align="center" gap="5" justify="center">
-{/*               <Button size="2" variant={"outline"}>
-                <a href="/login">Log in</a>
-              </Button> */}
-              <LoginDialog />
-              {/*<Button size="2" variant={"classic"}> MIGHT BE USED LATER FOR SIGN UP PAGE
-                <a href="/">Sign up</a>
-              </Button>*/}
             </Flex>
           </Box>
         )}
