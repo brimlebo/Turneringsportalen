fcbf050a968e921fd6c8e89421ed62003f29bbb0
Fredrik Lauritzen <l.fredrik@icloud.com>
2025-04-01 15:06:28 +0200

    feat/temp edit tournament page

--- frontend/app/tournaments/[tournament_id]/edit/page.tsx
+++ frontend/app/tournaments/[tournament_id]/edit/page.tsx
@@ -1,10 +1,11 @@
 import EditTournamentView from "@/components/editTournament/EditTournamentView";
 
+
 type Props = {
   params: { tournament_id: string };
 };
 
-export default async function EditTournamentPage({ params }: Props) {
-  const id = Number(params.tournament_id); 
-  return <EditTournamentView id={id} />;
+export default async function EditTournamentPage(props : {params: Promise <{tournament_id: string}> }) {
+  const params = await props.params; 
+  return <EditTournamentView id={parseInt(params.tournament_id)} />;
 }
